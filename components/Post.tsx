70ab455956e6eaf59d4a0c24eee8f24f95a9f200
marcogomesr <marcogomesr@gmail.com>
2022-07-31 18:19:15 +0200

    added votes

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Avatar from './Avatar'
 import {
   ArrowDownIcon,
@@ -12,12 +12,74 @@ import {
 import moment from 'moment'
 import Link from 'next/link'
 import { Jelly } from '@uiball/loaders'
+import toast from 'react-hot-toast'
+import { useSession } from 'next-auth/react'
+import { GET_ALL_VOTES_BY_POST_ID } from '../graphql/queries'
+import { useMutation, useQuery } from '@apollo/client'
+import { ADD_VOTE } from '../graphql/mutations'
 
 type Props = {
   post: Post
 }
 
 const Post = ({ post }: Props) => {
+  const [vote, setVote] = useState<boolean>()
+  const { data: session } = useSession()
+
+  const { data, loading } = useQuery(GET_ALL_VOTES_BY_POST_ID, {
+    variables: {
+      post_id: post?.id,
+    },
+  })
+
+  const [addVote] = useMutation(ADD_VOTE, {
+    refetchQueries: [GET_ALL_VOTES_BY_POST_ID, 'getPostListByPostId'],
+  })
+
+  const upVote = async (isUpvote: boolean) => {
+    if (!session) {
+      toast('You must be logged in to vote!')
+      return
+    }
+
+    if (vote && isUpvote) return
+    if (vote === false && !isUpvote) return
+
+    console.log('votin...')
+
+    await addVote({
+      variables: {
+        post_id: post?.id,
+        username: session.user?.name,
+        upvote: isUpvote,
+      },
+    })
+  }
+
+  useEffect(() => {
+    const votes: Vote[] = data?.getVotesByPostId
+
+    const vote = votes?.find(
+      (vote) => vote.username === session?.user?.name
+    )?.upvote
+
+    setVote(vote)
+  }, [data])
+
+  const displayVote = (data: any) => {
+    const votes: Vote[] = data?.getVotesByPostId
+    const displayNumber = votes?.reduce(
+      (total, vote) => (vote.upvote ? (total += 1) : (total -= 1)),
+      0
+    )
+    if (votes?.length === 0) return 0
+    if (displayNumber === 0) {
+      return votes[0]?.upvote ? 1 : -1
+    }
+
+    return displayNumber
+  }
+
   if (!post)
     return (
       <div className="flex items-center justify-center w-full p-10 text-xl">
@@ -30,9 +92,19 @@ const Post = ({ post }: Props) => {
       <div className="rounded-md flex cursor-pointer border bg-white border-gray-300 bg-white- shadow-sm hover:border hover:border-gray-600">
         {/* votes */}
         <div className="flex flex-col items-center justify-start space-y-1 rounded-l-md bg-gray-50 p-4 text-gray-400">
-          <ArrowUpIcon className="voteButtons hover:text-red-400" />
-          <p className="text-xs font-bold text-black">0</p>
-          <ArrowDownIcon className="voteButtons hover:text-blue-400" />
+          <ArrowUpIcon
+            onClick={() => upVote(true)}
+            className={`voteButtons hover:text-red-400 ${
+              vote && 'text-red-400'
+            }`}
+          />
+          <p className="text-xs font-bold text-black">{displayVote(data)}</p>
+          <ArrowDownIcon
+            onClick={() => upVote(false)}
+            className={`voteButtons hover:text-blue-400 ${
+              vote === false && 'text-bloue-400'
+            }`}
+          />
         </div>
 
         <div className="p-3 pb-1">
--- pages/post/[postid].tsx
+++ pages/post/[postid].tsx
@@ -6,6 +6,9 @@ import Post from '../../components/Post'
 import { GET_ALL_POST_BY_ID } from '../../graphql/queries'
 import { SubmitErrorHandler, SubmitHandler, useForm } from 'react-hook-form'
 import { ADD_COMMENT } from '../../graphql/mutations'
+import toast from 'react-hot-toast'
+import Avatar from '../../components/Avatar'
+import moment from 'moment'
 
 type FormData = {
   comment: string
@@ -36,7 +39,20 @@ const Postpage = () => {
 
   const onSubmit: SubmitHandler<FormData> = async (data) => {
     //post comment
-    console.log(data)
+    console.log('router', router)
+    const notifications = toast.loading('Posting your comment...')
+    await addComment({
+      variables: {
+        post_id: router.query.postid,
+        username: session?.user?.name,
+        text: data.comment,
+      },
+    })
+    setValue('comment', '')
+
+    toast.success('Comment posted!', {
+      id: notifications,
+    })
   }
 
   return (
@@ -67,6 +83,31 @@ const Postpage = () => {
             Comment
           </button>
         </form>
+      </div>
+
+      <div className="-my-5 rounded-b-md border border-t-0 border-gray-300 bg-white py-5 px-10">
+        <hr className="py-2" />
+        {post?.comments?.map((comment) => (
+          <div
+            key={comment.id}
+            className="relative flex imtes-center space-x-2 space-y-5"
+          >
+            <hr className="absolute top-10 left-7 z-0 h-16 border" />
+            <div className="z-50">
+              <Avatar seed={comment.username} />
+            </div>
+
+            <div className="flex flex-col">
+              <p className="py-2 text-xs text-gray-400">
+                <span className="font-semibold text-gray-600">
+                  {comment.username}
+                </span>{' '}
+                • {moment(comment.created_at).fromNow()}
+              </p>
+              <p>{comment.text}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
